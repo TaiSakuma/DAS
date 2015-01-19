20a1d2e2783784b9124200dc1216b5af0bae6d88
Arne Draeger <adraeger@cern.ch>
2015-01-19 08:56:32 +0000

    update

--- RA2Exercises/Utils/Sample.h
+++ RA2Exercises/Utils/Sample.h
@@ -45,8 +45,8 @@ private:
 
 
 // Path to ntuple directory. Adapt for your environment.
-TString Sample::path_ = "root://eoscms//eos/cms/store/cmst3/group/das2014/SUSYJetsPlusMHT/ntuple/v4/";
-
+//TString Sample::path_ = "root://eoscms//eos/cms/store/cmst3/group/das2014/SUSYJetsPlusMHT/ntuple/v4/";
+TString Sample::path_ = "/data/shared/Long_Exercise_SUSY/ntuples/";
 TString Sample::fileNameFullSample(unsigned int id) {
   checkId(id);
 
